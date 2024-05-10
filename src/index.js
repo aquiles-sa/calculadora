140703f63fb120a534d2a9671ef91b5878c3a9a9
Aquiles <aquiles2ws@gmail.com>
2024-05-10 00:45:14 -0300

    fix:modificação no operador

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ let operators = ["=", "-", "+", "*", "/"];
 const addValuesDisplay = (btnValue) => {
   if (btnValue === "AC") {
     output = "";
-  } else if (btnValue === "CE") {
+  } else if (btnValue === "DE") {
     output = output.toString().slice(0, -1);
   } else if (btnValue === "=" && output !== "") {
     output = eval(output);
   } else if (output === "" && operators.includes(btnValue)) {
-    output = "0";
+    output = "";
   } else {
     output += btnValue;
   }
